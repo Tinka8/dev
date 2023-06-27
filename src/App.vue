17e03ac43c318d042ee39e705d54c2af6ac4f344
Tinka8 <lenmatka818@gmail.com>
2023-06-27 14:33:23 +0200

    index

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
     <div class="w-full min-h-screen pt-20 overflow-hidden bg-cute-pink">
         <div class="w-2/6 h-full mx-auto text-cute-black">
             <div class="flex flex-col">
-                <header class="py-16 text-5xl text-left">
+                <header class="py-10 text-5xl text-left">
                     <h1 class="text-black">> tina818</h1>
                 </header>
 
                 <div class="relative flex flex-col pb-10 divide-y-2 divide-black">
-                    <div class="py-1 pr-2">
+                    <div class="py-2 pr-2">
                         <div class="flex justify-between pb-1 text-sm text-cute-brown">
                             <div class="uppercase">high school</div>
                             <div>2019-2023 ⬩ N 48.311° / E 18.082°</div>
