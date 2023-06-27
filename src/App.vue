6dfdc3bb70fb9e6c206ade90b0bc9f1726b58880
Tinka8 <lenmatka818@gmail.com>
2023-06-27 13:38:41 +0200

    index

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,36 @@
 <template>
-    <div class="bg-cute-pink w-full h-full min-h-screen pt-20">
+    <div class="w-full min-h-screen pt-20 overflow-hidden bg-cute-pink">
         <div class="w-2/6 h-full mx-auto text-cute-black">
-            <div class="flex">
-                
-                <div class="border-b-2 border-black">
-                    <header class="py-16 text-left text-5xl">
-                        <h1 class="text-black">> tina818</h1>
-                    </header>
-                    
-                    <div class="border-r-2 border-black pr-2">
-                        <div class="flex justify-between text-sm text-cute-brown pb-1">
+            <div class="flex flex-col">
+                <header class="py-16 text-5xl text-left">
+                    <h1 class="text-black">> tina818</h1>
+                </header>
+
+                <div class="relative flex flex-col pb-10 divide-y-2 divide-black">
+                    <div class="py-1 pr-2">
+                        <div class="flex justify-between pb-1 text-sm text-cute-brown">
                             <div class="uppercase">high school</div>
                             <div>2019-2023 ⬩ N 48.311° / E 18.082°</div>
                         </div>
                         <h2 class="text-3xl">Gymnazium Parovska</h2>
-                        <div class="py-8">Aliquam erat volutpat. Phasellus lacinia mauris eu libero sagittis fermentum.</div>
-                        <div class="uppercase text-sm text-cute-brown pb-2">school, high school, graduated</div>
+                        <div class="py-8">Aliquam erat volutpat. Phasellus lacinia mauris eu libero sagittis fermentum.
+                        </div>
+                        <div class="pb-2 text-sm uppercase text-cute-brown">school, high school, graduated</div>
                     </div>
-                </div>   
 
-                <div class="">
-                    <!-- wrapper a rok -->
-                    <div class="h-3/4"></div>
-                    <div class="text-3xl">2023</div>
+                    <div class="relative">
+                        <div class="absolute translate-x-full -translate-y-1/2 -right-4">
+                            <!-- wrapper a rok -->
+                            <div class="h-3/4"></div>
+                            <div class="text-3xl">2023</div>
+                        </div>
+                    </div>
+
+                    <div class="w-0.5 bg-black h-[200vh] absolute right-0"></div>
                 </div>
-
             </div>
-            
-            
         </div>
     </div>
-    
-
 </template>
 
 <script setup>
